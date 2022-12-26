e91c35aee7badbfd7d191703a9629fb0d1f80fdd
Rushikhandarenotion <112479854+Rushikhandarenotion@users.noreply.github.com>
2022-12-26 14:19:11 +0530

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,29 @@
+variable "aws_region" {
+  description = "The AWS region to create things in."
+  default     = "ap-south-1"
+}
+
+variable "key_name" {
+  description = " SSH keys to connect to ec2 instance"
+  default     = "vprofile-key"
+}
+
+variable "instance_type" {
+  description = "instance type for ec2"
+  default     = "t2.micro"
+}
+
+variable "security_group" {
+  description = "Name of security group"
+  default     = "my-jenkins-security-group"
+}
+
+variable "tag_name" {
+  description = "Tag Name of for Ec2 instance"
+  default     = "my-ec2-instance"
+}
+
+variable "ami_id" {
+  description = "AMI for Ubuntu Ec2 instance"
+  default     = "ami-03d3eec31be6ef6f9"
+}
